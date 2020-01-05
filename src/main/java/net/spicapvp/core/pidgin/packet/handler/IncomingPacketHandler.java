046918fbb7821f9c8ee97100c2271b16da3081c4
notsmatch404 <46530214+notsmatch404@users.noreply.github.com>
2020-01-05 18:21:00 +0900

    Update IncomingPacketHandler.java

--- src/main/java/net/spicapvp/core/pidgin/packet/handler/IncomingPacketHandler.java
+++ src/main/java/net/spicapvp/core/pidgin/packet/handler/IncomingPacketHandler.java
@@ -5,11 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Identifies a method and friend type assigned to the method.
- * The only valid parameter types for a method that is annotated
- * by this annotation are String and Packet.
- */
 @Target({ ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface IncomingPacketHandler {
